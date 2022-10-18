c15df6f6b3c13b1bcdc27fd45292b320c17fd1c2
root <root@localhost.localdomain>
2022-10-18 11:13:08 +0800

    add gun

--- gunicorn_flask.py
+++ gunicorn_flask.py
@@ -4,6 +4,7 @@ workers = 4   # 获取cpu数量来设置进程数量
 backlog = 2048
 # worker_class = "gevent" #  默认为sync, 也可以使用eventlet, gevent, tornado, gthread
 worker_connections = 1000
+timeout=600
 daemon = False
 debug = True
 proc_name = 'app'         # Flask 主程序文件名
